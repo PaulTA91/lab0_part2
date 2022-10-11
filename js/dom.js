c44df2c9abd93c99e1d5d58156ec5d8ad3f731fb
PaulTA91 <parmst202@caledonian.ac.uk>
2022-10-11 17:34:25 +0100

    initial commit/textareas complete

---
+++ js/dom.js
@@ -0,0 +1,68 @@
+let currentNode;
+
+function addNewParagraph() {
+  currentNode = document.getElementById("paragraph1");
+  input = document.getElementById("textarea1").value;
+  const newNode = document.createElement("p");
+  newNode.appendChild(document.createTextNode(input));
+  currentNode.parentNode.insertBefore(newNode, currentNode);
+  // currentNode = document.getElementById("paragraph1");
+  // input = document.getElementById("textarea2").value;
+  // const newNode2 = document.createElement("p");
+  // newNode.appendChild(document.createTextNode(" " + input));
+  // currentNode.parentNode.insertBefore(newNode2, currentNode);
+}
+
+function domView() {
+  let i;
+  let nodeList;
+  let output = "";
+
+  //<p> elements
+  nodeList = document.getElementsByTagName("p");
+  output += "Paragraphs(" + nodeList.length + "):" + "<br>";
+  for (i = 0; i < nodeList.length; i++) {
+    if (nodeList[i].id != "") {
+      output += nodeList[i].id + "<br>";
+    } else {
+      output += "(no id)" + "<br>";
+    }
+  }
+
+  //input elements
+  nodeList = document.getElementsByTagName("input");
+  output += "Inputs(" + nodeList.length + "):" + "<br>";
+  for (i = 0; i < nodeList.length; i++) {
+    if (nodeList[i].id != "") {
+      output += nodeList[i].id + "<br>";
+    } else {
+      output += "(no id)" + "<br>";
+    }
+  }
+
+  nodeList = document.getElementsByTagName("td");
+  output += "Table Cells(" + nodeList.length + "):" + "<br>";
+  for (i = 0; i < nodeList.length; i++) {
+    if (nodeList[i].id != "") {
+      output += nodeList[i].id + "<br>";
+    } else {
+      output += "(no id)" + "<br>";
+    }
+  }
+
+  currentNode = document.getElementById("domview");
+  currentNode.innerHTML = output;
+  currentNode.style.visibility = "visible";
+}
+
+function domHide() {
+  currentNode = document.getElementById("domview");
+  currentNode.style.visibility = "hidden";
+}
+
+window.onload = function () {
+  document.getElementById("listdom").onclick = domView;
+  document.getElementById("hidedom").onclick = domHide;
+  document.getElementById("addpara").onclick = addNewParagraph;
+  document.getElementById("altercell").onclick = alterCell;
+};
